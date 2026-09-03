f77b05bada836f03cf05a66d72d3edad671cedd8
flyahn06 <flyahn06@gmail.com>
2026-09-03 19:43:33 +0900

    fix: add rejection rule for specific recruitment type

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -2,8 +2,9 @@
 async function verify_request(req, res, next) {
     const field = req.body?.['field'];
     const exclude = req.body?.['exclude'];
+    const rctype = req.body?.['recruitment_type']
 
-    if (!field && !exclude) {
+    if (!field && !exclude && rctype != "어학병" && rctype != "카투사") {
         res.status(400).send("Necessary request body is empty.");
         return;
     }
